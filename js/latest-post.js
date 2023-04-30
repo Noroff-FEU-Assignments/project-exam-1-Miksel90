64e808b9a1955e65a14825a75fac191a7b1a2b0a
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-04-30 18:35:57 +0200

    dada

--- js/latest-post.js
+++ js/latest-post.js
@@ -4,58 +4,54 @@ const jsonBase  = "/wp-json/wp/v2";
 const postEndpoint = "/posts"; 
 
 // Full URL 
-const fullPostURL = apiBase + jsonBase + postEndpoint;
+const fullPostURL = apiBase + jsonBase + postEndpoint + "?_embed";
 
 // Fetching the products
 async function getLatestPost() {
   const response = await fetch(fullPostURL);
-
   const latestPosts = await response.json();
-
- return latestPosts;
-// console.log(latestPosts);
+  return latestPosts;
 }
+
 // Create HTML
 function createPostHTML(latestPost) {
-    const container = document.querySelector(".latest-container");
+  const container = document.querySelector(".latest-container");
 
-    const productContainer = document.createElement("a");
-    productContainer.href = "blog-specific.html?id=" + latestPost.id;
-    productContainer.classList.add("posts");
-    productContainer.id = latestPost.id;
+  const productContainer = document.createElement("a");
+  productContainer.href = "blog-specific.html?id=" + latestPost.id;
+  productContainer.classList.add("posts");
+  productContainer.id = latestPost.id;
 
-    const title = document.createElement("h4");
-    title.innerText = latestPost.title.rendered;
-    productContainer.append(title);
+  const title = document.createElement("h4");
+  title.innerText = latestPost.title.rendered;
+  productContainer.append(title);
 
-    // const featuredImage = latestPost._embedded["wp:featuredmedia"][0];
-    // if (featuredImage) {
-    //   const image = document.createElement("img");
-    //   image.src = featuredImage.source_url;
-    //   productContainer.append(image);
-    // }
+  // Check if post has a featured image
+  if (latestPost._embedded && latestPost._embedded["wp:featuredmedia"]) {
+    const featuredMedia = latestPost._embedded["wp:featuredmedia"];
+    if (featuredMedia.length > 0) {
+      const featuredImage = featuredMedia[0];
+      const image = document.createElement("img");
+      image.src = featuredImage.source_url;
+      productContainer.append(image);
+    }
+  }
 
-   /*  const quote = document.createElement("h5")
-    quote.innerText = latestPost.content.rendered;
-    productContainer.append(quote);
- */
-    
   container.append(productContainer);
 }
 
 function createPostsHTML(latestPosts) {
-    for (let i = 0; i < latestPosts.length; i++) {
-      const latestPost = latestPosts[i];
-      createPostHTML(latestPost);
-    }}
-
+  for (let i = 0; i < latestPosts.length; i++) {
+    const latestPost = latestPosts[i];
+    createPostHTML(latestPost);
+  }
+}
 
 // Create the main function
 async function main() {
-    const latestPosts = await getLatestPost();
-    createPostsHTML(latestPosts);
-  }
-
+  const latestPosts = await getLatestPost();
+  createPostsHTML(latestPosts);
+}
 
 // Run the entire function
-main()
+main();
