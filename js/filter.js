e0a29d06191ef55b3541c0814828e743afc25d2b
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-05-22 13:12:10 +0200

    searchbar

--- js/filter.js
+++ js/filter.js
@@ -1,38 +1,12 @@
- // Function to sort blog posts
- function sortPosts(sortBy) {
+const searchBar =document.querySelector(".searchbar");
+searchBar.addEventListener("keyup", filterPosts);
 
-  }
+function filterPosts(){
+    let filterValue = searchBar.value.toLowerCase();
+    // console.log(filterValue);
 
-  // Function to filter blog posts
-  function filterPosts(selectedFilters) {
-
-  }
-
-  // Function to search blog posts
-  function searchPosts(query) {
-  
-  }
-
-  // Event listeners for sorting, filtering, and searching
-  document.getElementById('sortSelect').addEventListener('change', function () {
-    var sortBy = this.value;
-    sortPosts(sortBy);
-  });
-
-  var checkboxes = document.querySelectorAll('#filterContainer input[type="checkbox"]');
-  checkboxes.forEach(function (checkbox) {
-    checkbox.addEventListener('change', function () {
-      var selectedFilters = [];
-      checkboxes.forEach(function (checkbox) {
-        if (checkbox.checked) {
-          selectedFilters.push(checkbox.value);
-        }
-      });
-      filterPosts(selectedFilters);
-    });
-  });
-
-  document.getElementById('searchInput').addEventListener('input', function () {
-    var query = this.value.toLowerCase();
-    searchPosts(query);
-  });
+    const searchPosts = document.querySelectorAll(".posts").forEach(post => {
+       post.innerText.toLowerCase().indexOf(filterValue) > -1
+        ? post.style.display = "" : post.style.display = "none";
+    })
+}
