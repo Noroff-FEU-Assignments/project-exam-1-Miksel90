56918b4ff3407174a617c94052523b1c3dfa0043
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-05-24 10:50:53 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,32 @@
 
-function createModal(){
-    const images = document.querySelectorAll("img");
-    // console.log(images);
-    const modal = document.getElementById("modal");
-    const modalImage = document.getElementById("modal-image");
-    const closeBtn = document.getElementsByClassName("close")[0];
+// function createModal(){
+//     const images = document.querySelectorAll("img");
+//     // console.log(images);
+//     const modal = document.getElementById("modal");
+//     const modalImage = document.getElementById("modal-image");
+//     const closeBtn = document.getElementsByClassName("close")[0];
   
-    images.forEach(function(image) {
-      image.addEventListener("click", function() {
-        modal.style.display = "block";
-        modalImage.src = this.src;
-        modalImage.alt = this.alt;
+//     images.forEach(function(image) {
+//       image.addEventListener("click", function() {
+//         modal.style.display = "block";
+//         modalImage.src = this.src;
+//         modalImage.alt = this.alt;
 
-      });
-    });
+//       });
+//     });
   
-    closeBtn.addEventListener("click", function() {
-      modal.style.display = "none";
-    });
+//     closeBtn.addEventListener("click", function() {
+//       modal.style.display = "none";
+//     });
   
-    window.addEventListener("click", function(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
-  }
+//     window.addEventListener("click", function(event) {
+//       if (event.target === modal) {
+//         modal.style.display = "none";
+//       }
+//     });
+//   }
   
 
 
-  export default createModal;
+//   export default createModal;
 
